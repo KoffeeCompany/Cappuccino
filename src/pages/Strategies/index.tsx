67e16279d9aa99b2169d56266230826c3a15650a
andryrkt1 <34197445+andryrkt1@users.noreply.github.com>
2021-12-02 13:17:31 +0100

    modify the gui according Olympus GUI

--- src/pages/Strategies/index.tsx
+++ src/pages/Strategies/index.tsx
@@ -1,107 +1,57 @@
-import { BottomRowContainer, FeatureCard, TopLeftContainer, TopRightContainer, TopRowContainer } from './featureCard'
-import {
-  BaseTokenLabel,
-  DescWrapper,
-  MainInfo,
-  MainInfoTitle,
-  NetworkContainer,
-  PriceLabel,
-  Tick,
-  TickContainer,
-  TickLabel,
-  TicksContainer,
-  TokenBaseQuoteTokensContainer,
-} from './featureTokenPairCard'
-import { NetworkContainer as IconContainer } from './multichainIcons'
-import {
-  ContentRow,
-  FeatureCardsContainer,
-  FeatureContainer,
-  LeftFillContainer,
-  MainContainer,
-  PageContainer,
-  RightFillContainer,
-  Search,
-  SearchBar,
-  SearchBarContainer,
-  SearchBarContent,
-  SearchInputWrapper,
-} from './styled'
-import './featureTokenPairCard.css'
-import { SearchInput } from 'components/SearchModal/styleds'
+import './style.css'
+import { ThemeContext } from 'styled-components/macro'
+import { MuiBox, MuiIcon } from './styled'
+import StickyHeadTable from './sticky-table'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import { useContext } from 'react'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
 
 export default function Strategies() {
+  const theme = useContext(ThemeContext)
   return (
     <>
-      <SearchBarContainer>
-        <SearchBarContent>
-          <SearchBar>
-            <Search>
-              <SearchInputWrapper>
-                <SearchInput placeholder="Search pools or tokens"></SearchInput>
-              </SearchInputWrapper>
-            </Search>
-          </SearchBar>
-        </SearchBarContent>
-      </SearchBarContainer>
-      <PageContainer>
-        <LeftFillContainer />
-        <MainContainer>
-          <ContentRow>
-            <FeatureContainer>
-              <FeatureCardsContainer>
-                <FeatureCard>
-                  <TopRowContainer>
-                    <TopLeftContainer>
-                      <TokenBaseQuoteTokensContainer>
-                        <BaseTokenLabel>ETH</BaseTokenLabel>
-                      </TokenBaseQuoteTokensContainer>
-                      <PriceLabel>$4,649.39</PriceLabel>
-                      <BaseTokenLabel className="subTitle">+4.45%</BaseTokenLabel>
-                    </TopLeftContainer>
-                    <TopRightContainer>
-                      <NetworkContainer>
-                        <IconContainer>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="12"
-                            height="20"
-                            fill="none"
-                            viewBox="0 0 12 20"
-                          >
-                            <path
-                              fill="#fff"
-                              fillRule="evenodd"
-                              d="M5.955 14.53L0 11.013l5.955 8.393 5.959-8.393-5.96 3.517h.001zM6.045 0L.09 9.882l5.955 3.521L12 9.886 6.045 0z"
-                              clipRule="evenodd"
-                            ></path>
-                          </svg>
-                        </IconContainer>
-                      </NetworkContainer>
-                    </TopRightContainer>
-                  </TopRowContainer>
-                  <BottomRowContainer>
-                    <DescWrapper>Generates yield by running an automated AAVE covered call strategy.</DescWrapper>
-                    <MainInfoTitle>Current Projected Yield (APY)</MainInfoTitle>
-                    <MainInfo>10.17%</MainInfo>
-                    <TicksContainer>
-                      <TickContainer>
-                        <TickLabel>Current Deposits</TickLabel>
-                        <Tick>3,946.31 AAVE</Tick>
-                      </TickContainer>
-                      <TickContainer>
-                        <TickLabel>Max Capacity</TickLabel>
-                        <Tick>15.5K AAVE</Tick>
-                      </TickContainer>
-                    </TicksContainer>
-                  </BottomRowContainer>
-                </FeatureCard>
-              </FeatureCardsContainer>
-            </FeatureContainer>
-          </ContentRow>
-        </MainContainer>
-        <RightFillContainer />
-      </PageContainer>
+      <Container maxWidth="lg">
+        <MuiBox className="strategies-title">
+          <Grid className="grid-side">
+            <div></div>
+          </Grid>
+          <Grid>
+            <MuiIcon>Ohmptions</MuiIcon>
+            <Typography
+              variant="h4"
+              gutterBottom
+              component="div"
+              className="typo-subTitle"
+              style={{ color: theme.text2 }}
+            >
+              Securing liquidity for protocols across DeFi
+            </Typography>
+          </Grid>
+          <Grid className="grid-side">
+            <div></div>
+          </Grid>
+        </MuiBox>
+      </Container>
+      <div className="plutus-view">
+        <Paper elevation={0} className="ohm-card" style={{ background: theme.bg0 }}>
+          <Box className="bond-title">
+            <Typography variant="h5" gutterBottom component="div" style={{ color: theme.text3 }}>
+              Total Liquidity Bonded
+            </Typography>
+            <Typography variant="h2" gutterBottom component="div" style={{ color: theme.text1, fontWeight: 'bold' }}>
+              $19,590,533
+            </Typography>
+          </Box>
+          <Grid className="grid-container bond-grid">
+            <Grid className="grid-container grid-row">
+              <StickyHeadTable></StickyHeadTable>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
     </>
   )
 }
